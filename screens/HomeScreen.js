cde8940b482c5f844ef7f1b3f44b5bbf634949e6
Kugan Priyan <88609808+kugan7370@users.noreply.github.com>
2021-11-21 23:57:36 +0530

    create header component commit

---
+++ screens/HomeScreen.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { View, Text } from 'react-native'
+import Header from '../components/home/Header'
+
+export default function HomeScreen() {
+    return (
+       <Header/>
+    )
+}
